004a821d2763b7563b70ce376c5aef7c4daab913
jthcast <jthcast@gmail.com>
2021-05-20 16:44:08 +0900

    Add LazyImage component

---
+++ components/LazyImage.js
@@ -0,0 +1,67 @@
+import intersectionObserver from '../utils/intersectionObserver.js';
+
+export default class LazyImage{
+  constructor({
+    alt,
+    className,
+    emptyImageSrc = `../public/empty-picture.png`,
+    height,
+    src,
+    target,
+    width
+  }){
+    this.img = document.createElement(`img`);
+    this.img.classList.add(`lazyimage`);
+    this.alt = alt;
+    this.className = className;
+    this.emptyImageSrc = emptyImageSrc;
+    if(this.emptyImageSrc){
+      const errorHandling = (event) => {
+        const target = event.target;
+          target.src = this.emptyImageSrc;
+      };
+      this.img.addEventListener(`error`, errorHandling);
+    }
+    this.height = height;
+    this.src = src;
+    this.width = width;
+    
+    const callback = (entry, observer) => {
+      if (entry.isIntersecting) {
+        const element = entry.target;
+        const dataSrc = element.getAttribute(`data-src`);
+        if(dataSrc){
+          const src = encodeURI(dataSrc);
+          element.setAttribute(`src`, src);
+        }
+        observer.unobserve(element);
+      }
+    };
+    intersectionObserver(this.img, callback, {
+      root: null, rootMargin: `50%`, threshold: 0
+    });
+    
+    target.appendChild(this.img);
+    this.render();
+  }
+
+  render(){
+    if(this.alt){
+      this.img.alt = alt;
+    }
+    if(this.className){
+      this.img.classList.add(this.className);
+    }
+    if(this.height){
+      this.img.height = this.height;
+    }
+    if(this.src){
+      this.img.setAttribute(`data-src`, this.src);
+    }else{
+      this.img.src = this.emptyImageSrc;
+    }
+    if(this.width){
+      this.img.width = this.width;
+    }
+  }
+}
