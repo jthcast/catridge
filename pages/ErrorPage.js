d6f5240bec29bb5232ff0e37a8b0c488ba78b1c5
jthcast <jthcast@gmail.com>
2021-06-02 15:12:19 +0900

    Add Error page

---
+++ pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import ErrorSection from '../components/ErrorSection.js';
+
+const ErrorPage = ({ target }) => {
+  const $ErrorSection = new ErrorSection({
+    target,
+  });
+};
+
+export default ErrorPage;
