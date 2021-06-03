0882c4d8615520d70579c8b640bdbb24b85377a0
jthcast <jthcast@gmail.com>
2021-06-04 01:36:22 +0900

    Fix add darkmodeSwitch

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Footer from '../components/Footer.js';
 import Header from '../components/Header.js';
 import { initRouter } from '../utils/router.js';
+import Switch from '../components/Switch.js';
+import setState from '../utils/setState.js';
+import Icon from '../components/Icon.js';
 
 const App = (root) => {  
   const header = new Header({
@@ -20,6 +23,34 @@ const App = (root) => {
   const footer = new Footer({
     target: root,
     copyright: { name: `Jthcast`, href: `https://jthcast.dev`},
+  });
+
+  const iconSun = new Icon({
+    target: root,
+    icon: `sun`
+  });
+
+  const iconMoon = new Icon({
+    target: root,
+    icon: `moon`
+  });
+
+  let isDark = false;
+  const darkmodeHandling = () => {
+    isDark = !isDark;
+    const colorMode = isDark ? `dark` : `light`;
+    
+    document.documentElement.setAttribute(`data-theme`, colorMode);
+    setState(darkmodeSwitch, { isChecked: isDark });
+  };
+
+  const darkmodeSwitch = new Switch({
+    className: `switch-darkmode`,
+    target: root,
+    unCheckedChildren: iconSun.dom,
+    checkedChildren: iconMoon.dom,
+    isChecked: false,
+    onClick: darkmodeHandling
   });
 };
 
